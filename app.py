bc5104014af515da6c6267c7c8707c7584974a92
Akbar H. Harahap <48922889+Qhiba@users.noreply.github.com>
2025-01-07 22:05:30 +0700

    gradio update

--- app.py
+++ app.py
@@ -3,12 +3,23 @@ import gradio as gr
 def greet(name, intensity):
     return "Hello, " + name + "!" * int(intensity)
 
+with gr.Blocks() as interface:
+    gr.Markdown('# Greetings from Gradio!')
+    inp = gr.Textbox(placeholder="What is your name?")
+    out = gr.Textbox()
 
-interface = gr.Interface(
-    fn=greet,
-    inputs=["text", "slider"],
-    outputs=["text"]
-)
+    inp.change(
+        fn = lambda x: f"Welcome, {x}",
+        inputs = inp,
+        outputs = out
+    )
+
+    gr.Interface(
+        fn=greet,
+        inputs=['text', 'slider'],
+        outputs=['text']
+    )
 
 
-app = interface.app
+if __name__ == "__main__":
+    interface.launch(inbrowser=True)
--- run.bat
+++ run.bat
@@ -1,6 +1,6 @@
 @echo off
 call .env\Scripts\activate
-uvicorn app:app --reload --port 8080
+gradio app.py --demo-name=interface
 
 if %errorlevel% neq 0 (
     echo ERROR: Run update.bat first to ensure the environment is set up.
